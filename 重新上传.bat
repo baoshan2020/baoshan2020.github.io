09d981e48b44c6ea81434cf6e568af99d8c801f7
mabaoshan2020 <398068229@qq.com>
2020-03-22 21:03:37 +0800

    autoupdate

--- 重新上传.bat
+++ 重新上传.bat
@@ -4,5 +4,10 @@ cd puma
 hugo --buildDrafts
 hugo --theme=jane --baseUrl="mabaoshan.top"
 cd public
+git add .
+git commit -m "autoupdate"
+
+git remote rm origin
+
 git remote add origin https://github.com/baoshan2020/baoshan2020.github.io.git
 git push -u origin master
